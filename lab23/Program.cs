830c54e72cbdabcfd71d03e1964dcf07e3f8b6dd
Cross Swetlana <crossswetlana.99@outlook.com>
2020-11-03 10:00:15 +0530

    Add project files.

---
+++ lab23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab23
+{
+    class Cityname
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the no. of cities:");
+            int size = Convert.ToInt32(Console.ReadLine());
+            string[] cities = new string[size];
+            Console.WriteLine("enter the city name");
+
+            for (int i=0; i < size;i++)
+                {
+                cities[i] = Console.ReadLine();
+                }
+            Console.WriteLine("--cities--");
+            foreach(string city in cities)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine(city);
+            }
+            Console.ReadLine();
+        }
+    }
+}
